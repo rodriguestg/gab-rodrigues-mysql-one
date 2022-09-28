1f6e0add248188528dce9e9c20c6939b2dcc0fdb
rodriguestg <grs.ibr@gmail.com>
2022-09-28 10:29:46 -0300

    Subindo requisito 2

--- desafio2.sql
+++ desafio2.sql
@@ -1,1 +1,3 @@
-SELECT COUNT(id_cancoes) AS cancoes, MAX(artista_id_artista) AS artistas, MAX(album_id_album) AS albuns FROM SpotifyClone.cancoes;
+SELECT COUNT(id_cancoes) AS cancoes, COUNT(DISTINCT id_artista) AS artistas, COUNT(DISTINCT id_album) AS albuns FROM SpotifyClone.cancoes
+INNER JOIN SpotifyClone.album ON album_id_album=id_album
+INNER JOIN SpotifyClone.artista ON artista_id_artista=id_artista;
