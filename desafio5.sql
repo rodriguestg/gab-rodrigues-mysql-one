f1fd823da901f75e2374c912704248705dd7540b
rodriguestg <grs.ibr@gmail.com>
2022-09-28 14:52:52 -0300

    Subindo requisito 6 e ajuste no 5

--- desafio5.sql
+++ desafio5.sql
@@ -1,5 +1,4 @@
 SELECT cancoes AS cancao, COUNT(cancoes_id_cancoes) AS reproducoes FROM SpotifyClone.cancoes
 INNER JOIN SpotifyClone.reproducao ON cancoes_id_cancoes=id_cancoes
-INNER JOIN SpotifyClone.artista ON cancoes.artista_id_artista=id_artista
 GROUP BY cancoes
-ORDER BY reproducoes DESC LIMIT 2;
+ORDER BY reproducoes LIMIT 10 OFFSET 7;
---
+++ desafio9.sql
@@ -0,0 +1,2 @@
+SELECT COUNT(usuario_id_usuario) AS quantidade_musicas_no_historico FROM SpotifyClone.reproducao
+WHERE usuario_id_usuario=1;
